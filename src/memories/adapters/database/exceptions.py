6555fa1f808134b9cc996aaa81bcd82f7e1baf78
poorpen <roma.polovik.03@mail.ru>
2023-11-28 23:28:16 +0300

    Added message for exceptions

--- src/memories/adapters/database/exceptions.py
+++ src/memories/adapters/database/exceptions.py
@@ -2,10 +2,12 @@ from memories.applications.common.exceptions import ApplicationException
 
 
 class CommitError(ApplicationException):
+    @property
     def message(self) -> str:
         return "An error occurred during the commit"
 
 
 class RollbackError(ApplicationException):
+    @property
     def message(self) -> str:
         return "An error occurred during the rollback"
--- src/memories/adapters/identity_provider/basic.py
+++ src/memories/adapters/identity_provider/basic.py
@@ -10,16 +10,16 @@ from memories.applications.common.exceptions import ApplicationException, Access
 from memories.adapters.database.models.user import user
 
 
-class AuthStringError(ApplicationException):
-    pass
-
-
 class MissingEmailOrPassword(ApplicationException):
-    pass
+    @property
+    def message(self) -> str:
+        return "Password or name has been omitted from the authentication string"
 
 
 class MissingBasic(ApplicationException):
-    pass
+    @property
+    def message(self) -> str:
+        return 'The word "basic" is missing from the authentication string'
 
 
 class BasicIdentityProvider(IdentityProvider):
@@ -38,13 +38,13 @@ class BasicIdentityProvider(IdentityProvider):
     def _decode_string(self) -> Tuple[str, str]:
         split_string = self._auth_string.strip().split(" ")
 
-        if split_string[0].lower() != "basic":
+        if not split_string or split_string[0].lower() != "basic":
             raise MissingBasic()
 
         try:
             email, password = b64decode(split_string[1]).decode().split(":")
             return email, password
-        except ValueError as exc:
+        except (ValueError, IndexError) as exc:
             raise MissingEmailOrPassword from exc
 
     def _get_user(self, email: str) -> Optional[RowMapping]:
